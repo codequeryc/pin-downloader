60d161b716ab63196391069b9d4800ce4d72d063
codequeryc <contact.thesquadhub@gmail.com>
2025-06-25 18:26:44 +0500

    Update fetch.js

--- api/fetch.js
+++ api/fetch.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
   const { url } = req.query;
   if (!url) return res.status(400).json({ success: false, error: "URL is required" });
 
